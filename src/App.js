26cd9133d20a25b5d411ac456f3495a9f8eea2e7
Pedro Laxe <pedrolaxe@gmail.com>
2022-04-01 02:30:56 -0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [apiData, setapiData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => setapiData(data));
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <br/>
+
+      {
+        apiData.map(data => 
+        <div key={data.id}>
+          Name: {data.name}<br/>
+          username: {data.username}<br/>
+          email: {data.email}<br/>
+          <br/>
+        </div>
+        )
+      }
     </div>
   );
 }
